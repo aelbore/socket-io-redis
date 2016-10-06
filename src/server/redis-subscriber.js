a823bd01e8313ac573c0735e246be766abec2d46
Martin Llaneza <martinllaneza@gmail.com>
2016-10-06 18:14:23 +0800

    Update redis-subscriber.js

--- src/server/redis-subscriber.js
+++ src/server/redis-subscriber.js
@@ -1,26 +1,16 @@
 import * as redis from 'redis';
 
 var client = redis.createClient();
-var EVENT_SET = '__keyevent@0__:set';
-var EVENT_DEL = '__keyevent@0__:del';
 
 // For this to work, remember to set notify-keyspace-events correctly in your redis configuration (Eg$ will suffice)
 // More info on notify-keyspace-events on http://redis.io/topics/notifications
 let redisSubscriber = {
-    listen: () => {
+    listenWithSocket: (ioSocket) => {
         client.on('pmessage', function(pattern, channel, key) {
-            console.log(pattern, channel, key);
-            switch (channel) {
-                case EVENT_SET:
-                    console.log('Key "' + key + '" set!');
-                break;
-                case EVENT_DEL:
-                    console.log('Key "' + key + '" deleted!');
-                break;
-            }
+            console.log('Room: ' + channel);
+            ioSocket.to(channel).emit('occupations', key);
         });
-        // subscribes the client to all patterns
-        client.psubscribe('*'); 
+        client.psubscribe('*'); // subscribes the client to all patterns
     }
 }
 
