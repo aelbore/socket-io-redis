e97c03300da6a9521df168f3b77713a0514430e6
Martin Llaneza <martinllaneza@gmail.com>
2016-10-06 18:06:21 +0800

    Update socket-io.js

--- src/server/socket-io.js
+++ src/server/socket-io.js
@@ -1,41 +1,24 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import * as io from 'socket.io';
-import * as redis from 'redis';
 import { Occupations } from './occupations';
 import { OccupationSeed } from './occupations-seed';
 import { AppSubscriber } from './app-subscriber';
 
-let redisClient = redis.createClient();
-
 let socketio = {
   onInit: (server) => {
     let ioSocket = io.listen(server);
-    //OccupationSeed();
+    AppSubscriber.listenWithSocket(ioSocket);
 
-    /// initialize occupations demo;
-    /// Occupations.onInit(socket);
-
-    AppSubscriber.subscribe(ioSocket);
-    
     ioSocket.sockets.on('connection', (socket) => {
       /// listen to the emit events from client
       socket.on('message', (data) => {
-        let clientId = socket.client.id;
-        /// check the key exist or the new connection
-        // if (!socketio.isExistKey(clientId)){
-        //   redisClient.set(socket.client.id, 'demo');
-        // }
         /// print the message key
-        console.log(`> Client: ${clientId}, data: ${data}`);
+        console.log(`> Client: ${socket.client.id}, data: ${JSON.stringify(data)}`);
+        data.forEach((value) => {
+          socket.join(value);
+        });
       });
-    });
-  },
-  isExistKey: (key) => {
-    let client = redis.createClient();
-    client.get(key, (err, value) => {
-      if (!(value)) { return false; }
-      return true;
     });
   }
 };
